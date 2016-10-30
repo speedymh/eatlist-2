dba86a03886737e19034d3381cd2e510646c4100
speedymh <matthias_hoppe@outlook.de>
2016-10-30 23:20:57 +0100

    sass edits

--- js/function.js
+++ js/function.js
@@ -1,3 +1,12 @@
 $(function(){
+	$cardHeight = $('.card').height();
+	$logoWidth = $('.logo').width();
 	
+	$wrapperHeight = $cardHeight*2
+	$cardWidth = $logoWidth+80
+	
+	console.log($cardHeight, $logoWidth);
+	
+	$('.wrapper').css('height', $cardHeight);
+	$('.card').css('width', $cardWidth);
 });
